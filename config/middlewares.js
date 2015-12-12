5feba3dc7991ae0c1925ecdf8f4915e2a171aa96
Vitor Mattos <vitor.mattos@vtex.com.br>
2015-12-12 21:18:14 -0200

    É É É

--- config/middlewares.js
+++ config/middlewares.js
@@ -14,7 +14,7 @@ var middlewares = function(app) {
   app.use(function enableCors(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*')
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-    res.header("Access-Control-Allow-Headers", "*, Content-Type")
+    res.header("Access-Control-Allow-Headers", "*, Content-Type, Accept")
     if (req.method == "OPTIONS") {
       return res.send(200).end()
     } else {
