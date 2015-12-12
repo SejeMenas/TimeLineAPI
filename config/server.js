656ea933b744d96a5d4aea3d933339707010ec22
Vitor Mattos <vitor.mattos@vtex.com.br>
2015-12-12 18:20:09 -0200

    keiros > kairos

--- config/server.js
+++ config/server.js
@@ -3,9 +3,9 @@ var server = {
     appToken: "tB1QxLWj3y1P",
     secret: "PpzYRV36c9K2"
   },
-  keirosApi: {
+  kairosApi: {
     appId: "0491f0f7",
-    keirosKey: "6cb9262f8062483489a9d22b750751d0"
+    kairosKey: "6cb9262f8062483489a9d22b750751d0"
   }
 }
 
