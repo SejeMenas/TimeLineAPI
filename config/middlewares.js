ebc89362444047b666c9f54e5c5a21176c4dd1e7
Vitor Mattos <vitor.mattos@vtex.com.br>
2015-12-12 21:03:21 -0200

    FODA-SE VOU ATÉ COMMITAR DE NOVO MAIS UMA VEZ

--- config/middlewares.js
+++ config/middlewares.js
@@ -13,7 +13,7 @@ var middlewares = function(app) {
 
   app.use(function enableCors(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*')
-    res.header("Access-Control-Allow-Methods", "GET, OPTIONS")
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     if (req.method == "OPTIONS") {
       res.send(200).end()
