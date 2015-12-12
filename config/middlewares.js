aef44f466efe7598d957c6fa5375b4fbcf3b5afe
Vitor Mattos <vitor.mattos@vtex.com.br>
2015-12-12 21:12:32 -0200

    FODASSE MESMO de novo

--- config/middlewares.js
+++ config/middlewares.js
@@ -14,6 +14,7 @@ var middlewares = function(app) {
   app.use(function enableCors(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*')
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+    res.header("Access-Control-Allow-Headers", "*")
     if (req.method == "OPTIONS") {
       return res.send(200).end()
     } else {
