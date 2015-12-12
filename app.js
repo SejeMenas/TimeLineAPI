be4c8977121a94e8a8012e09762512704a7e62d2
Vitor Mattos <vitor.mattos@vtex.com.br>
2015-12-11 22:20:12 -0200

    config

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 
 var app = express();
 
+var config = require('./config/server')
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
---
+++ config/server.js
@@ -0,0 +1,8 @@
+var server = {
+  transpApi: {
+    appToken: process.env.TRANS_BR_APPTOKEN,
+    secret: process.env.TRANS_BR_SECRET
+  }
+}
+
+module.exports = server
