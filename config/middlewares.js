3a15a0cdd1db89a113dbfb995761066e5877944b
Vitor Mattos <vitor.mattos@vtex.com.br>
2015-12-12 20:49:12 -0200

    dibbily doobely

--- config/middlewares.js
+++ config/middlewares.js
@@ -13,7 +13,7 @@ var middlewares = function(app) {
 
   app.use(function enableCors(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*')
-    res.header("Access-Control-Allow-Methods", "GET, OPTIONS")
+    res.header("Access-Control-Allow-Methods", "GET, POST")
     res.header("Access-Control-Allow-Credentials", "true")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
