3c27cc59dc6cf35c4aa81fe40268fd4d291b070e
Vitor Mattos <vitor.mattos@vtex.com.br>
2015-12-12 17:17:39 -0200

    Keiros api keys

--- config/server.js
+++ config/server.js
@@ -2,6 +2,10 @@ var server = {
   transApi: {
     appToken: "tB1QxLWj3y1P",
     secret: "PpzYRV36c9K2"
+  },
+  keirosApi: {
+    appId: "0491f0f7",
+    keirosKey: "6cb9262f8062483489a9d22b750751d0"
   }
 }
 
