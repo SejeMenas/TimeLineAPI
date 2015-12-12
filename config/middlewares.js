9f752e60c571c08dda27b8e4926a12ee93f434d7
Vitor Mattos <vitor.mattos@vtex.com.br>
2015-12-12 16:16:58 -0200

    CORS

--- config/middlewares.js
+++ config/middlewares.js
@@ -10,6 +10,17 @@ var middlewares = function(app) {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(cookieParser());
   app.use(express.static(path.join(__dirname, 'public')));
+
+  app.use(function enableCors(config) {
+    return function (req, res, next) {
+      res.header("Access-Control-Allow-Origin", '*')
+      res.header("Access-Control-Allow-Methods", "GET, OPTIONS")
+      res.header("Access-Control-Allow-Credentials", "true")
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+      next()
+    }
+  })
+
 }
 
 module.exports = middlewares
